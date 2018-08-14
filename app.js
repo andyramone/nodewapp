9fa7836db418a52bc3a14db7720ff48865f8410f
andyramone <awylon@gmail.com>
2018-08-14 12:26:23 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,56 @@
 var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-http.createServer(function(req, res){
-	//request, response
-	res.writeHead(202,'You win'); //create header
-	res.end('Executed OK'); //payload?
-}).listen(80); //define listening port
+http.createServer(function(req, res) {
+    var queryData = url.parse(req.url, true);
+    if (queryData.pathname === '/') {
+        res.writeHead(200);
+        res.end('Welcome to calc! Allowed operations are add, subtract, multiple.');
+    }
+    else if (queryData.pathname === '/add')
+    {
+        var data = {
+            a: queryData.query.a,
+            b: queryData.query.b,
+            operand: 'add',
+            result: Number(queryData.query.a) + Number(queryData.query.b)
+        };
+    
+        res.writeHead(200, {'content-type': 'application/json'});
+        res.end(JSON.stringify(data));
+    }
+    else if (queryData.pathname === '/subtract')
+    {
+        var data = {
+            a: Number(queryData.query.a),
+            b: Number(queryData.query.b),
+            operand: 'subtract',
+            result: Number(queryData.query.a) - Number(queryData.query.b)
+        };
+    
+        res.writeHead(200, {'content-type': 'application/json'});
+        res.end(JSON.stringify(data));
+    }
+    else if (queryData.pathname === '/multiply')
+    {
+        var data = {
+            a: queryData.query.a,
+            b: queryData.query.b,
+            operand: 'multiply',
+            result: Number(queryData.query.a) * Number(queryData.query.b)
+        };
+    
+        res.writeHead(200, {'content-type': 'application/json'});
+        res.end(JSON.stringify(data));
+    }
+    else
+    {
+        res.writeHead(404);
+        res.end('Not found');
+    }
 
-console.log('Server started');
+
+}).listen(80);
+
+console.log('Server running...');
